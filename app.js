62687ec405f198c138c39b5b808a9f4df4bc9335
Elaine Ho <elaine48e@gmail.com>
2020-02-19 13:21:03 -0800

    added calling api and displaying images on index

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
+const https = require('https');
 const express = require('express')
 const app = express();
 
 const port = 3000;
 
+var films;
+
+https.get('https://api.jikan.moe/v3/producer/21', (res) => {
+    console.log("Called API");
+    let data = '';
+    res.on('data', (chunk) => {
+        data += chunk;
+    });
+    res.on('end', function(){
+        films = JSON.parse(data);
+    });
+});
+
+
 app.set('view engine', 'ejs');
+app.use(express.static("public"));
+
+
 
 app.get('/', function(req, res){
-    res.render('pages/index');
+    res.render('pages/index',{
+        films: films.anime
+    });
 });
 
 app.listen(port);
