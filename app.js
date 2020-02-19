219e604fda66a25667099ec055c921eb298069bb
Elaine Ho <elaine48e@gmail.com>
2020-02-18 20:36:15 -0800

    Setting up the pages

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express();
+
+const port = 3000;
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+    res.render('pages/index');
+});
+
+app.listen(port);
