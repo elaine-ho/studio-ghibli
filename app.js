30e30e02e140254186f6a20f7380f2ef176fb9cc
Elaine Ho <elaine48e@gmail.com>
2020-02-21 14:47:43 -0800

    Created detail page and added pagination

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express();
 
 const port = 3000;
 
+const pageLimit = 16;
+
 var films;
 
 https.get('https://api.jikan.moe/v3/producer/21', (res) => {
@@ -24,8 +26,27 @@ app.use(express.static("public"));
 
 
 app.get('/', function(req, res){
+    let page;
+    if (!req.query.page){
+        page = 1;
+    }
+    else{
+        page = req.query.page;
+    }
+    const startIndex = (page-1) * pageLimit;
+    const endIndex = page * pageLimit;
+    const totalPages = Math.ceil(films.anime.length / pageLimit);
+    const animes = films.anime.slice(startIndex, endIndex);
     res.render('pages/index',{
-        films: films.anime
+        films: animes,
+        page: page,
+        totalPages: totalPages
+    });
+});
+
+app.get('/detail', function(req, res){ 
+    res.render('pages/detail',{
+        id:req.query.id
     });
 });
 
