945da7d008389090de2122a2031f5f96a792eaa5
Elaine Ho <elaine48e@gmail.com>
2020-02-23 00:13:59 -0800

    Got async to work for detail page

--- app.js
+++ app.js
@@ -3,13 +3,12 @@ const express = require('express')
 const app = express();
 
 const port = 3000;
-
 const pageLimit = 16;
+const apiUrl = "https://api.jikan.moe/v3/";
 
 var films;
-
-https.get('https://api.jikan.moe/v3/producer/21', (res) => {
-    console.log("Called API");
+https.get(apiUrl + 'producer/21', (res) => {
+    console.log("Called API for producer/21");
     let data = '';
     res.on('data', (chunk) => {
         data += chunk;
@@ -44,10 +43,57 @@ app.get('/', function(req, res){
     });
 });
 
-app.get('/detail', function(req, res){ 
+function lookup(id){
+    return new Promise((resolve, reject) =>[
+        https.get(apiUrl + 'anime/' + films.anime[id].mal_id, (res) => {
+            console.log("Called API for anime/" + films.anime[id].mal_id);
+            let data = '';
+            res.on('data', (chunk) => {
+                data += chunk;
+            });
+            res.on('end', function(){
+                resolve(JSON.parse(data));
+            });
+        }).on('error', function(e) {
+            reject(e);
+        })
+    ]);
+}
+
+function characters(id){
+    return new Promise((resolve, reject) =>[
+        https.get(apiUrl + 'anime/' + films.anime[id].mal_id + '/characters_staff', (res) => {
+            console.log("Called API for " + films.anime[id].mal_id + "/characters_staff");
+            let data = '';
+            res.on('data', (chunk) => {
+                data += chunk;
+            });
+            res.on('end', function(){
+                resolve(JSON.parse(data));
+            });
+        }).on('error', function(e) {
+            reject(e);
+        })
+    ]);
+}
+
+const asyncMiddleware = fn =>(req, res, next) => {
+    Promise.resolve(fn(req, res, next)).catch(next);
+};
+
+app.get('/detail', asyncMiddleware(async (req, res) => { 
+    var id = req.query.id;
+    if (!films.anime[id].lookup){
+        films.anime[id].lookup = await lookup(id);
+        
+    }
+    if (!films.anime[req.query.id].characters){
+        films.anime[id].characters = await characters(id);
+    }
     res.render('pages/detail',{
-        id:req.query.id
+        lookup: films.anime[req.query.id].lookup,
+        characters: films.anime[req.query.id].characters
     });
-});
+}));
 
 app.listen(port);
