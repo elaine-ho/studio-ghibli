5bcd665d7e45db04cc23c29473209ce12c422792
Elaine Ho <elaine48e@gmail.com>
2020-02-29 14:31:23 -0800

    Added another test case

--- tests/test.js
+++ tests/test.js
@@ -4,6 +4,17 @@ module.exports = {
         .url('http://localhost:3000/')
         .waitForElementVisible('body')
         .assert.titleContains('Studio Ghibli Archive')
+        .assert.containsText('main', 'Howl no Ugoku Shiro')
+        .end();
+    },
+    'Test Clicking on Howl no Ugoku Shiro' : function (browser) {
+      browser
+        .url('http://localhost:3000/')
+        .waitForElementVisible('body')
+        .useXpath() 
+        .click("//a[text()='Howl no Ugoku Shiro']")
+        .useCss()  
+        .assert.containsText('main', "Howl's Moving Castle")
         .end();
     }
   };
